8d4f1816bfd6634c263277c2b1eab456b1ce94f7
IuKim <s3805504@rmit.edu.vn>
2022-05-09 08:26:43 +0700

    location server

---
+++ src/test/main/LocationServerTest.java
@@ -0,0 +1,27 @@
+package main;
+
+import helper.LocationWorkerPrx;
+import org.junit.Test;
+import support.LocationDetails;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class LocationServerTest {
+    public static List<LocationDetails> cityInfo = ContextManager.readCityInfo();
+    public static LocationWorkerPrx locationWorker;
+    public static final String INDOOR = "Indoor";
+    public static final String OUTDOOR = "Outdoor";
+
+    @Test
+    public void testCheckIndoorOrOutdoor() {
+        String[] expected_status = {INDOOR, INDOOR, OUTDOOR, OUTDOOR};
+
+        assertEquals(4, cityInfo.size());
+        for (int i = 0; i < cityInfo.size(); i++) {
+            String locationStatus = locationWorker.locationMapping(cityInfo.get(i).getLocation());
+            assertEquals(expected_status[i], locationStatus);
+        }
+    }
+}
