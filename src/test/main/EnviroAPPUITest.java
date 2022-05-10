9e7d22117860921b82b1a9867df32c3946095d36
anhduc2708 <68212055+anhduc2708@users.noreply.github.com>
2022-05-10 08:48:38 +0700

    update unit test for APPUI and integration test for UI_CM

---
+++ src/test/main/EnviroAPPUITest.java
@@ -0,0 +1,27 @@
+package main;
+
+import helper.Alert;
+import org.junit.Test;
+import support.LocationDetails;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+
+public class EnviroAPPUITest {
+
+    @Test
+    public void testTemperatureAlert() {
+//
+//        String[] locat = {"A","B","C","D"};
+//        Alert alert = new Alert("Temperature", 20, locat);
+//        StringBuilder expectedOutput = new StringBuilder("Warning,Temperature is now: " + alert.value + "\n");
+//        expectedOutput.append("Suggestion - please go to: " + "\n");
+//        for (String location : alert.locations) {
+//            expectedOutput.append(location).append(" , ");
+//        }
+//
+//       assertEquals(expectedOutput.toString(),alert("Temperature",alert.value, alert.locations));
+
+        }
+}
+
+
