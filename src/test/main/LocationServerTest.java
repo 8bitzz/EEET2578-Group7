996427e177bf6bf54ca62568b27e7e379f50d5bc
IuKim <s3805504@rmit.edu.vn>
2022-05-12 17:28:16 +0700

    LStest basics

--- src/test/main/LocationServerTest.java
+++ src/test/main/LocationServerTest.java
@@ -1,27 +1,42 @@
 package main;
 
-import helper.LocationWorkerPrx;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import support.LocationDetails;
 
+import java.util.LinkedHashMap;
 import java.util.List;
-
 import static org.junit.Assert.*;
 
 public class LocationServerTest {
-    public static List<LocationDetails> cityInfo = ContextManager.readCityInfo();
-    public static LocationWorkerPrx locationWorker;
     public static final String INDOOR = "Indoor";
     public static final String OUTDOOR = "Outdoor";
+    public static LinkedHashMap<String, String> locationTable;
+
+    @BeforeClass
+    public static void setUpClass() {
+        SetupTest.setupService();
+    }
 
     @Test
     public void testCheckIndoorOrOutdoor() {
+        List<LocationDetails> cityInfo = ContextManager.readCityInfo();
         String[] expected_status = {INDOOR, INDOOR, OUTDOOR, OUTDOOR};
 
         assertEquals(4, cityInfo.size());
         for (int i = 0; i < cityInfo.size(); i++) {
-            String locationStatus = locationWorker.locationMapping(cityInfo.get(i).getLocation());
+            String locationStatus = ContextManager.locationWorker.locationMapping(cityInfo.get(i).getLocation());
             assertEquals(expected_status[i], locationStatus);
         }
+    }
+
+    @Test
+    public void testCheckLSReadInfo() {
+//        locationTable = LocationServer.table;
+//
+//        for (int i = 0; i < 4; i++) {
+//            String locationStatus = ContextManager.locationWorker.locationMapping(cityInfo.get(i).getLocation());
+//            assertEquals(expected_status[i], locationStatus);
+//        }
     }
 }
