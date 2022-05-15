48561450b9f4a88bf3138fb831b56c9716210f77
Hang Nguyen <hanu.nguyenhang@gmail.com>
2022-05-15 13:28:47 +0700

    [r] Refractor code for Location Sever and Location Server Worker

--- src/test/main/UnitTestingLocationServer.java
+++ src/test/main/UnitTestingLocationServer.java
@@ -4,24 +4,22 @@ import helper.LocationWorkerPrx;
 import org.junit.Test;
 import support.LocationDetails;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class UnitTestingLocationServer {
-    public static List<LocationDetails> cityInfo = ContextManager.readCityInfo();
-    public static LocationWorkerPrx locationWorker;
     public static final String INDOOR = "Indoor";
     public static final String OUTDOOR = "Outdoor";
 
     @Test
-    public void testCheckIndoorOrOutdoor() {
-        String[] expected_status = {INDOOR, INDOOR, OUTDOOR, OUTDOOR};
-
-        assertEquals(4, cityInfo.size());
-        for (int i = 0; i < cityInfo.size(); i++) {
-            String locationStatus = locationWorker.locationMapping(cityInfo.get(i).getLocation());
-            assertEquals(expected_status[i], locationStatus);
-        }
+    public void testReadLocationConfigFile() {
+        System.out.println("\nTest LS read Location Config file");
+        LinkedHashMap<String, String> actualResult = LocationServer.readConfig();
+        assertEquals(INDOOR, actualResult.get("A"));
+        assertEquals(INDOOR, actualResult.get("B"));
+        assertEquals(OUTDOOR, actualResult.get("C"));
+        assertEquals(OUTDOOR, actualResult.get("D"));
     }
 }
--- src/test/main/UnitTestingLocationWorker.java
+++ src/test/main/UnitTestingLocationWorker.java
@@ -1,7 +1,53 @@
 package main;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static main.ContextManager.communicator;
 import static org.junit.Assert.*;
 
 public class UnitTestingLocationWorker {
+    private final LocationServer.LocationWorkerI locationWorker = new LocationServer.LocationWorkerI();
+    public static final String INDOOR = "Indoor";
+    public static final String OUTDOOR = "Outdoor";
 
+    @Before
+    public void setUpLocationWorker() {
+        communicator = com.zeroc.Ice.Util.initialize();
+        ContextManager.iniLocationMapper();
+        LocationServer.table = LocationServer.readConfig();
+    }
+
+    @Test
+    public void testMappingKeyIndoorLocation() {
+        System.out.println("\nTest LocationWorker mapping INDOOR location");
+        String[] expectedLocations = {INDOOR, INDOOR};
+        String[] locationList ={"A", "B"};
+
+        for (int i = 0; i < locationList.length; i++) {
+            assertEquals(expectedLocations[i], locationWorker.locationMapping(locationList[i], null));
+        }
+    }
+
+    @Test
+    public void testMappingKeyOutdoorLocation() {
+        System.out.println("\nTest LocationWorker mapping OUTDOOR location");
+        String[] expectedLocations = {OUTDOOR, OUTDOOR};
+        String[] locationList ={"C", "D"};
+
+        for (int i = 0; i < locationList.length; i++) {
+            assertEquals(expectedLocations[i], locationWorker.locationMapping(locationList[i], null));
+        }
+    }
+
+    @Test
+    public void testMappingInvalidLocation() {
+        System.out.println("\nTest LocationWorker mapping INVALID location");
+        String[] expectedLocations = {null, null};
+        String[] locationList ={"E", "F"};
+
+        for (int i = 0; i < locationList.length; i++) {
+            assertEquals(expectedLocations[i], locationWorker.locationMapping(locationList[i], null));
+        }
+    }
 }
