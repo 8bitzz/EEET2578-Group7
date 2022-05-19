486ed53765d7d3f13ae5c22578bfacc9357fb9e5
IuKim <s3805504@rmit.edu.vn>
2022-05-19 16:03:09 +0700

    more cases

--- src/test/main/JUnitSensor.java
+++ src/test/main/JUnitSensor.java
@@ -22,21 +22,33 @@ public class JUnitSensor {
     @Test
     public void testSensorGetUsername() {
         assertEquals("Jack", data.getUsername());
+        data.setUsername("David");
+        assertEquals("David", data.getUsername());
     }
 
     @Test
     public void testSensorSetUsername() {
         data.setUsername("David");
         assertEquals("David", data.getUsername());
+        data.setUsername("Jack");
+        assertEquals("Jack", data.getUsername());
     }
 
     @Test
     public void testSensorGetType() {
         assertEquals("Location", data.getType());
+        data.setType("Temperature");
+        assertEquals("Temperature", data.getType());
+        data.setType("AQI");
+        assertEquals("AQI", data.getType());
     }
 
     @Test
     public void testSensorSetType() {
+        data.setType("Location");
+        assertEquals("Location", data.getType());
+        data.setType("Temperature");
+        assertEquals("Temperature", data.getType());
         data.setType("AQI");
         assertEquals("AQI", data.getType());
     }
